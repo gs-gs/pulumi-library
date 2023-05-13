6f79426283cccd36e003dbea09550ec4861e6139
jrburcher <jrburcher@gmail.com>
2023-05-13 21:35:16 +1000

    ci: update

--- node/components/aws/CloudfrontWebsite.ts
+++ node/components/aws/CloudfrontWebsite.ts
@@ -212,8 +212,4 @@ export class CloudfrontWebsite extends pulumi.ComponentResource {
   public cloudfrontAliases(): pulumi.Output<string[] | undefined> {
     return this.cloudfrontDistribution.aliases;
   }
-
-  public cloudfrontARN(): pulumi.Output<string | undefined> {
-    return this.cloudfrontDistribution.arn;
-  }
 }
